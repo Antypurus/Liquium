a24bc655b71e3e23d33e014b35ac437c7115244a
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-10-03 01:27:33 +0100

    WIP

--- Tests/test_main.cpp
+++ Tests/test_main.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
 
-void main()
+TEST(string_tests, test1)
 {
-    GTEST_ASSERT_EQ(1, 1);
+    EXPECT_EQ(1,1);
+    EXPECT_EQ(2,2);
+}
+
+TEST(string_tests, test2)
+{
+    EXPECT_EQ(3,3);
+    EXPECT_EQ(4,3);
 }
