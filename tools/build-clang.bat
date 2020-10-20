c0006a4c18640d29bfa7f8ad841a8e3bd425e442
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-10-20 16:35:12 +0100

    fixing clang output binary extension

--- tools/build-clang.bat
+++ tools/build-clang.bat
@@ -3,6 +3,4 @@
 cmake -E env LDFLAGS="-fuse-ld=lld-link" PATH="C:\ProgramData\chocolatey\lib\ninja\tools"
 cmake -H. -G Ninja -Bbuild/clang -DCMAKE_C_COMPILER:PATH="%ProgramFiles%\LLVM\bin\clang.exe" -DCMAKE_CXX_COMPILER:PATH="%ProgramFiles%\LLVM\bin\clang.exe" -DCMAKE_C_COMPILER_ID="Clang" -DCMAKE_CXX_COMPILER_ID="Clang" -DCMAKE_SYSTEM_NAME="Generic"
 cd build/clang
-del Liquium.exe
 ninja
-ren Liquium Liquium.exe
