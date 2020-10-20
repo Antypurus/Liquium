eb87f1d76323d616da738057e2c4a605c32fa7b8
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-10-20 04:16:44 +0100

    refactoring project structure

--- build-clang.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cmake -E env LDFLAGS="-fuse-ld=lld-link" PATH="C:\ProgramData\chocolatey\lib\ninja\tools"
-cmake -H. -G Ninja -Bbuild -DCMAKE_C_COMPILER:PATH="%ProgramFiles%\LLVM\bin\clang.exe" -DCMAKE_CXX_COMPILER:PATH="%ProgramFiles%\LLVM\bin\clang.exe" -DCMAKE_C_COMPILER_ID="Clang" -DCMAKE_CXX_COMPILER_ID="Clang" -DCMAKE_SYSTEM_NAME="Generic"
-cd build
-ninja
