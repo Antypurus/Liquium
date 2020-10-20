a5481e908cc5afcd39c4209a43ddb24e4b7f6085
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-10-20 22:38:26 +0100

    WIP

--- tools/build-clang.bat
+++ tools/build-clang.bat
@@ -2,7 +2,7 @@
 Rem this script must be called from the liquium project root
 Rem this script run the cmake configuration script and compiles the project into an executable
 
-cmake -E env LDFLAGS="-fuse-ld=lld-link" PATH="C:\ProgramData\chocolatey\lib\ninja\tools"
+Rem cmake -E env LDFLAGS="-fuse-ld=lld-link" PATH="C:\ProgramData\chocolatey\lib\ninja\tools"
 cmake -H. -G Ninja -Bbuild/clang -DCMAKE_C_COMPILER:PATH="%ProgramFiles%\LLVM\bin\clang.exe" -DCMAKE_CXX_COMPILER:PATH="%ProgramFiles%\LLVM\bin\clang.exe" -DCMAKE_C_COMPILER_ID="Clang" -DCMAKE_CXX_COMPILER_ID="Clang" -DCMAKE_SYSTEM_NAME="Generic"
 cd build/clang
 ninja
