8a7e27362d9ece359d84b96a6163d90f709656cc
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-10-20 21:30:17 +0100

    static analysis should now work well

--- tools/run-static-analysis.bat
+++ tools/run-static-analysis.bat
@@ -3,8 +3,9 @@ Rem this script should be called from the liquium project root
 Rem this script will run static analysis via clang tidy
 
 set "directory=%cd%"
-call ./config-clang-tidy.bat
+call tools/config-clang-tidy.bat
 cd build/clang
-python ../../CI/Static Analysis/run-clang-tidy.py
+xcopy "..\\..\\.clang-tidy*" ".clang-tidy*" /y
+python ../../CI/StaticAnalysis/run-clang-tidy.py
 cd %directory%
 
