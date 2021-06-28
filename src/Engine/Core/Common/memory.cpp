306880d234d0e1e7a46483826c30118cabd804b5
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2021-06-28 22:24:03 +0100

    placeholder implementation of memory management

--- src/Engine/Core/Common/memory.cpp
+++ src/Engine/Core/Common/memory.cpp
@@ -1,8 +1,25 @@
 #include "memory.hpp"
 
+#include <stdlib.h>
+
 namespace liq
 {
 	
+	void* alloc(uint64 size)
+	{
+		return malloc(size);
+	}
 	
+	void* realloc(void* ptr, uint64 new_size)
+	{
+		// TODO(Tiago): we might want to have some checks for null here
+		return ::realloc(ptr, new_size);
+	}
+	
+	void free(void* ptr)
+	{
+		// TODO(Tiago): might wanna have some checks for null here
+		::free(ptr);
+	}
 	
 }
--- src/Engine/Core/Common/memory.hpp
+++ src/Engine/Core/Common/memory.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "types.hpp"
+
+//TODO(Tiago): for now everything here is placeholder and needs to be substituted by our own allocator, based on windows memory management i guess. ( we might want to have a win_memory, so that we can also have a linux_mem for server side support)
+namespace liq
+{
+	
+	void* alloc(uint64 size);
+	void* realloc(void* ptr, uint64 new_size);
+	void free(void* ptr);
+	
+}
